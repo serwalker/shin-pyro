669b0906c09ea1fdc0618b910fdfca0e976cf657
serwalker <105328357+serwalker@users.noreply.github.com>
2022-09-04 10:23:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 from base64 import b64decode
+from distutils.util import strtobool
 from os import getenv
 
 from dotenv import load_dotenv
@@ -15,21 +16,23 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡️")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/9ffcecae609a1aae73971.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
     BLACKLIST_CHAT = [-1001473548283]
-BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
-BOT_VER = "0.2.0@main"
+BOT_VER = "0.4.0@main"
 BRANCH = "main"
 CHANNEL = getenv("CHANNEL", "oh_shinnn")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("Z2hwX1NEMWJ0T2phQ3Y3RFBXQmtHeUVCUTkyZWlKN09YVzFBVEY1RA==").decode(
+    b64decode("Z2hwXzg1Z1I1RlRRanRRTG9TU2FaRGVrcnI4c1pBSDB6cjRRMlN5UA==").decode(
         "utf-8"
     ),
 )
@@ -37,6 +40,7 @@ GROUP = getenv("GROUP", "nsjsvsjsnbsksjs")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
 REPO_URL = getenv(
     "REPO_URL",
     b64decode("aHR0cHM6Ly9naXRodWIuY29tL3Byb2plY3RtNG4vUHlyb01hbg==").decode("utf-8"),
